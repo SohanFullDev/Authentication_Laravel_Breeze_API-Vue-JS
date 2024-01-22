27a434bab4ea3d3ba209032a126d0e84e9ed9663
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-22 10:10:39 +0600

    login & displaying user info_b

--- vue-breeze-frontend/src/components/Home.vue
+++ vue-breeze-frontend/src/components/Home.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const user = ref();
+onMounted(async () => {
+    const data = await axios.get("/api/user");
+    console.log(data);
+
+});
+</script>
 <template>
     <div>
         <h1 class="text-3xl font-bold text-orange-500">Welcome to the Home Page</h1>
